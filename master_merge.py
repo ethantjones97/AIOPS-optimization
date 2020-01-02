1cb13fe51b358509a4afa53a4c363f9ac2211fd9
Jones <etjones@deloitte.com>
2020-01-02 12:07:48 -0500

    nye update

--- master_merge.py
+++ master_merge.py
@@ -1,5 +1,3 @@
-
-
 #%%
 #import libraries
 import pandas as pd 
@@ -9,16 +7,16 @@ from ortools.graph import pywrapgraph
 # update to your local repository
 data = pd.read_csv("C:/Users/etjones/Desktop/AI OPS/AIOPS-optimization/Transportation_Network.csv") 
 
-list1 = data.start.unique();
-list2 = data.end.unique();
+list1 = data.start.unique()
+list2 = data.end.unique()
 
 L = list(set().union(list1, list2))
 print(list1)
 print(list2)
 print(L)
 
-NodeDict = { i : L[i] for i in range(0, len(L) ) }
-print('NodeDict:', NodeDict,'/n')
+NodeDict = {i: L[i] for i in range(0, len(L))}
+print('NodeDict:', NodeDict, '/n')
 
 start_nodes = []
 end_nodes = []
@@ -39,28 +37,28 @@ for key, value in NodeDict.items():
         if end_nodes[i] == value:
             end_nodes[i] = key
 
-print("start nodes:", start_nodes,"\n")
-print("end nodes:",end_nodes,"\n")
-print("capacities:",capacities,"\n")
-print("unit costs:",unit_costs,"\n")
+print("start nodes:", start_nodes, "\n")
+print("end nodes:", end_nodes, "\n")
+print("capacities:", capacities, "\n")
+print("unit costs:", unit_costs, "\n")
 #%%
 edges = []
 for ii in range(len(start_nodes)):
-    edges.append((start_nodes[ii],end_nodes[ii]))
+    edges.append((start_nodes[ii], end_nodes[ii]))
 #    edges.append((start_nodes[ii],end_nodes[ii],{'capacity': capacities[ii]},{'cost': costs[ii]}))
 print("edges:", edges, "\n")
 
-G=nx.DiGraph()
+G = nx.DiGraph()
 G.add_edges_from(edges)
 
-H=nx.relabel_nodes(G,NodeDict)
+H = nx.relabel_nodes(G, NodeDict)
 pos = nx.spring_layout(H)
-nx.draw(H,pos,with_labels=True)
+nx.draw(H, pos, with_labels=True)
 plt.show()
 #%%
 # CHEATING and definina all resources at the source to start. we need to find a way to have the user input this in the csv, then we can ingest it. 
 
-supplies = [0,0,0,0,0,0,0,0,0,10000,0,0]
+supplies = [0, 0, 0, 0, 0, 0, 0, 0, 0, 10000, 0, 0]
 
 #cast to python integers from NumPy integers:
 capacities = [int(i) for i in capacities]
