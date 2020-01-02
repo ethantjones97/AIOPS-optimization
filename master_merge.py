41abdddc5ec11431e968081536eec1c388004b95
Jones <etjones@deloitte.com>
2020-01-02 14:50:23 -0500

    ethan updates 1/2

--- master_merge.py
+++ master_merge.py
@@ -1,14 +1,19 @@
 #%%
-#import libraries
 import pandas as pd 
+from time import process_time
 import networkx as nx
 import matplotlib.pyplot as plt
 from ortools.graph import pywrapgraph
+#%%
+t1_start = process_time()
+#import libraries
 # update to your local repository
-data = pd.read_csv("C:/Users/etjones/Desktop/AI OPS/AIOPS-optimization/Transportation_Network.csv") 
-
-list1 = data.start.unique()
-list2 = data.end.unique()
+data_connections = pd.read_csv("C:/Users/etjones/Desktop/AI OPS/AIOPS-optimization/Transportation_Network.csv") 
+data_supply = pd.read_csv("C:/Users/etjones/Desktop/AI OPS/AIOPS-optimization/supply.csv") 
+data_demand = pd.read_csv("C:/Users/etjones/Desktop/AI OPS/AIOPS-optimization/demand.csv") 
+#%%
+list1 = data_connections.start.unique()
+list2 = data_connections.end.unique()
 
 L = list(set().union(list1, list2))
 print(list1)
@@ -16,18 +21,18 @@ print(list2)
 print(L)
 
 NodeDict = {i: L[i] for i in range(0, len(L))}
-print('NodeDict:', NodeDict, '/n')
+print('NodeDict:', NodeDict, '\n')
 
 start_nodes = []
 end_nodes = []
 capacities = []
 unit_costs = []
 
-for index, row in data.iterrows():
-    start_nodes.append(data['start'][index])
-    end_nodes.append(data['end'][index])
-    capacities.append(data['capacities'][index])
-    unit_costs.append(data['unit costs'][index])
+for index, row in data_connections.iterrows():
+    start_nodes.append(data_connections['start'][index])
+    end_nodes.append(data_connections['end'][index])
+    capacities.append(data_connections['capacities'][index])
+    unit_costs.append(data_connections['unit costs'][index])
     
 for key, value in NodeDict.items():
     for i in range(len(start_nodes)):
@@ -58,32 +63,75 @@ plt.show()
 #%%
 # CHEATING and definina all resources at the source to start. we need to find a way to have the user input this in the csv, then we can ingest it. 
 
-supplies = [0, 0, 0, 0, 0, 0, 0, 0, 0, 10000, 0, 0]
+supply_nodes = []
+supply_values = []
+for index, row in data_supply.iterrows():
+    supply_nodes.append(data_supply['node'][index])
+    supply_values.append(data_supply['supply'][index])
 
+for key, value in NodeDict.items():
+    for i in range(len(supply_nodes)):
+        if supply_nodes[i] == value:
+            supply_nodes[i] = key
+    for i in range(len(supply_values)):
+        if supply_values[i] == value:
+            supply_values[i] = key
+
+demand_nodes = []
+demand_values = []
+for index, row in data_demand.iterrows():
+    demand_nodes.append(data_demand['node'][index])
+    demand_values.append(data_demand['demand'][index])
+
+for key, value in NodeDict.items():
+    for i in range(len(demand_nodes)):
+        if demand_nodes[i] == value:
+            demand_nodes[i] = key
+    for i in range(len(demand_values)):
+        if demand_values[i] == value:
+            demand_values[i] = key
+
+combo_nodes = supply_nodes
+combo_values = []
+for ii in range(len(combo_nodes)): 
+    combo_values.append(supply_values[ii] + demand_values[ii]) 
+
+#%%
 #cast to python integers from NumPy integers:
 capacities = [int(i) for i in capacities]
 unit_costs = [int(i) for i in unit_costs]
+combo_nodes = [int(i) for i in combo_nodes]
+combo_values = [int(i) for i in combo_values]
 
 min_cost_flow = pywrapgraph.SimpleMinCostFlow()
 for ii in range(0, len(start_nodes)):
     min_cost_flow.AddArcWithCapacityAndUnitCost(start_nodes[ii], end_nodes[ii], capacities[ii], unit_costs[ii])
-for i in range(0, len(supplies)):
-    min_cost_flow.SetNodeSupply(i, supplies[i])
+for ii in range(0, len(combo_values)):
+    min_cost_flow.SetNodeSupply(combo_nodes[ii], combo_values[ii])
 #%%
-print(min_cost_flow.OPTIMAL)
-print(min_cost_flow.Solve())
+#print(min_cost_flow.OPTIMAL)
+#print(min_cost_flow.Solve())
 #%%
+    
+solution_lol = [] #initiate solution list of lists
 if min_cost_flow.Solve() == min_cost_flow.OPTIMAL:
-    print('Minimum cost:', min_cost_flow.OptimalCost())
+    print('\n' + 'Minimum cost:', min_cost_flow.OptimalCost())
     print('')
-    print('  Arc    Flow / Capacity  Cost')
     for i in range(min_cost_flow.NumArcs()):
       cost = min_cost_flow.Flow(i) * min_cost_flow.UnitCost(i)
-      print('%1s -> %1s   %3s  / %3s       %3s' % (
-          min_cost_flow.Tail(i),
-          min_cost_flow.Head(i),
-          min_cost_flow.Flow(i),
-          min_cost_flow.Capacity(i),
-          cost))
+      solution_lol.append([NodeDict[min_cost_flow.Tail(i)],NodeDict[min_cost_flow.Head(i)]
+                            ,min_cost_flow.Flow(i), min_cost_flow.Capacity(i),cost])
+      col_names = ['From','To','Flow','Capacity','Cost']
+      solution_df = pd.DataFrame(solution_lol,columns = col_names)
+
 else:
     print('There was an issue with the min cost flow input.')
+
+export_csv = solution_df.to_csv(r'C:\Users\etjones\Desktop\AI OPS\AIOPS-optimization\solution_dataframe.csv',index=False) 
+print(solution_df)    
+print('\n', "Time =", process_time() - t1_start, "seconds")
+
+# RECOMMENDED NEXT STEPS:
+# 1. Validate results using AMPL
+# 2. Improve Network visualization
+# 3. Visualize optimal flow network
